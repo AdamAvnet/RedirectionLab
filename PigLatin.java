d3f55993ca375a6ef63e5e072bf5c5acc28eb551
AdamAvnet <aavnet20@stuy.edu>
2020-12-23 14:26:24 -0500

    Fixed the extra line at the end

--- PigLatin.java
+++ PigLatin.java
@@ -21,7 +21,8 @@ public class PigLatin{
 				System.out.print(pigLatinSimple(latin));
 				System.out.print(" ");
 			}
-			System.out.println();
+			if(Sca.hasNextLine())
+				System.out.println();
 		}
 	}
 
